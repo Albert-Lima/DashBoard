b980989b9b719f60984f622bf075d9c8b04ff011
Francisco Albert <albertsousalima@gmail.com>
2023-10-25 23:34:39 -0300

    estrutura completa

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
 var ContainerTrabalhos = document.querySelector('#trabalhos')
 var trabalhos = document.createElement('div')
-var trabalhosPart1 = document.createElement('div')
-var trabalhosPart2 = document.createElement('div')
-var Texto1 = ["job", "client", "date"]
-var Texto2 = ["received"]
+var DadosTrabalho = document.createElement('div')
+var ValorRecebido = document.createElement('div')
+var DJob = document.createElement('div')
+var DClient = document.createElement('div')
+var DData = document.createElement('div')
+var DRecebido = document.createElement('div')
 
-function AdicionarJobs(){
+trabalhos.setAttribute('id', 'cardJobs')
+DadosTrabalho.setAttribute('id', 'DadosTrabalho')
+ValorRecebido.setAttribute('id', 'ValorRecebido')
+DData.setAttribute('class', 'dadosJOBS')
+DClient.setAttribute('class', 'dadosJOBS')
+DJob.setAttribute('class', 'dadosJOBS')
+DRecebido.setAttribute('class', 'dadosJOBS')
+
+function adicionarElementos(){
     ContainerTrabalhos.appendChild(trabalhos)
+    trabalhos.appendChild(DadosTrabalho)
+    trabalhos.appendChild(ValorRecebido)
+
+    DadosTrabalho.appendChild(DJob)
+    DadosTrabalho.appendChild(DClient)
+    DadosTrabalho.appendChild(DData)
+    ValorRecebido.appendChild(DRecebido)
+
+    DJob.innerHTML = 'Job'
+    DClient.innerHTML = 'Client'
+    DData.innerHTML = 'Data'
+    DRecebido.innerHTML = 'Recebido'
 }
-window.addEventListener('load', AdicionarJobs)
+window.addEventListener('load', adicionarElementos)
